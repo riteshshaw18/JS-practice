97c5306bae5d212534d9dc4621a5af10d4b41b1a
Ritesh kumar shaw <108003597+riteshshaw18@users.noreply.github.com>
2024-01-18 07:57:59 +0000

    commit

--- practice/variable.js
+++ practice/variable.js
@@ -3,6 +3,7 @@ let named="ritesh" //always use it
 var dept="It" //it is not a good way to declare variable using var because 
 //it have issue named blocked and fuctional scope.
 year="4th"
+let state;//we have'nt assign value in it so it will show undefined.
 
 
 console.table([id,dept,named,year])
