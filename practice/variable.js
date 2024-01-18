ba89b4fab74e445ffffbae875131bc7b2274ff61
Ritesh kumar shaw <108003597+riteshshaw18@users.noreply.github.com>
2024-01-18 07:56:05 +0000

    container commit done

---
+++ practice/variable.js
@@ -0,0 +1,8 @@
+const id=2345//we can't change it
+let named="ritesh" //always use it 
+var dept="It" //it is not a good way to declare variable using var because 
+//it have issue named blocked and fuctional scope.
+year="4th"
+
+
+console.table([id,dept,named,year])
