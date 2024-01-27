aaf9d72b9bd0f39bf12e84ab2445d6034fbaba10
Ritesh kumar shaw <108003597+riteshshaw18@users.noreply.github.com>
2024-01-27 13:56:24 +0000

    memory

---
+++ practice/memory.js
@@ -0,0 +1,12 @@
+/*
+memory has 2 types:
+1.stack 2. heap
+
+
+1.) stack: primitive data type are stored in stack.
+here when we invoke the variable then we got copy not oriianl one ,
+so all changes are done in copied one and not in original one .
+
+2.) heap: nonprimitive data types are stored in heap.
+here when we invoke the variable we got the reference ,so any changes made are done in original.
+*/
